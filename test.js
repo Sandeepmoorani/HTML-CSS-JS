dcf148d4e0cf9b784b13c0caa88ce833755357a3
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-02-09 18:44:06 +0500

    Add files via upload

---
+++ test.js
@@ -0,0 +1,50 @@
+// document.write("HELLO WORLD <br><br>");
+// document.write("<i> <b>Hey Hello again </b></i>");
+
+// variables 
+
+// var x= "Welcome";
+// var y="to my";
+// var z="Website";
+
+// let firstname ="Sandeep";
+// const secondn="Moorani";
+
+// document.write(x);
+// document.write(y);
+// document.write(z);
+// document.write(firstname);
+// document.write(secondn);
+
+
+// datatypes 
+
+// var x= "Datatypes";
+// var x=15;
+// var x=false;
+// var y;
+// x=["html","cs","js"]
+// document.write(x);
+// document.write("<br>");
+// document.write(typeof x);
+
+// operations
+
+// var a=10;
+// var b=2;
+// var c=a+b;
+// var c=a-b;
+// var c=a*b;
+// var c=a/b;
+// var c=a%b;
+// a-=b;
+// a+=b;
+// a*=b;
+// document.write(a);
+
+// console
+
+var x=10;
+// console.log(x);
+console.log([1,2,3]);
+
